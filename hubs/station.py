0482212d9bc2e290368a0a77edf65f37bafb26d1
Phillip Lane <iamaperson620@gmail.com>
2022-12-06 22:15:07 -0600

    Adding station interface

--- hubs/station.py
+++ hubs/station.py
@@ -51,36 +51,85 @@ class Station:
 
         if self.occupied and self.timer < self.delay:
             innersignal = StationState.BOARD # Train waits at the station
-        elif self.timer >= self.delay: # If timer hits 10 counts 
+        elif self.timer >= self.delay and self.occupied: # If timer hits 10 counts 
             innersignal = StationState.EXIT # Train exits the station 
         
         return outersignal, innersignal
 
 class StationInterface:
-    def __init__(self):
-        self.station = Station()
+    def __init__(self, waittime):
+        self.station = Station(waittime)
         self.ports = [None, None]
         self.action_taken = (None, None)
+        self.internal = None
+        self.occupied = False
         
     def is_waiting(self, port):
-        self.ports[port] = ArriveEvent.ARRIVE
+        self.ports[port] = ArriveEvent.ARRIVE # If a train arrives at the station, it waits 
     
     def step(self):
-        pass
+        # Port[0] = entry    port[1] = exit
+        action = self.internal if self.internal else self.ports[0]
+        self.internal = None
+        self.action_taken = self.station.step(action) # Action taken
+        if self.action_taken[0] == StationState.GREEN: # If the station signal is green 
+            self.internal = ArriveEvent.ENTER # the train enters the station
+            self.ports[0] = None # Train has entered, noting in port[0] now 
+        if self.action_taken[1] == StationState.EXIT: # If a train exits the station
+            self.internal = ArriveEvent.LEAVE # The state at the station is that a train left
+        if self.occupied and self.station.occupied == False: # If the station is occupied
+            self.ports[1] = ArriveEvent.LEAVE # Tell the train to leave 
+        else:
+            self.ports[1] = None # If the station is not otherwise occupied, then nothing is leaving the station 
+        self.occupied = self.station.occupied
 
     def is_consumed(self, port):
-        return self.ports[port] == ArriveEvent.LEAVE
+        if port == 0: # The station becomes occupied 
+            return self.internal == ArriveEvent.ENTER # Train enters station 
+        else:
+            return False
     
     def is_produced(self, port):
-        return
+        if port == 1: # Station is emptied 
+            return self.ports[port] == ArriveEvent.LEAVE # Train leaves the station
+        else:
+            return False
 
+'''
 def testwait(station):
     state = station.step(None)
     while state[1] != StationState.EXIT:
         state = station.step(None)
         print(state)
+'''
+
+def testwait(stationInterface):
+    while not stationInterface.is_produced(1):
+        #for i in range(0, 5):
+        stationInterface.step()
+        print("Port 0: Consumed ({}), Produced ({})".format(stationInterface.is_consumed(0), stationInterface.is_produced(0)))
+        print("Port 1: Consumed ({}), Produced ({})".format(stationInterface.is_consumed(1), stationInterface.is_produced(1)))
+        print()
 
 def test():
+    interface = StationInterface(2)
+
+    interface.is_waiting(0)
+    testwait(interface)
+    
+    print();print()
+
+    interface.is_waiting(0)
+    interface.step()
+    print("Port 0: Consumed ({}), Produced ({})".format(interface.is_consumed(0), interface.is_produced(0)))
+    print("Port 1: Consumed ({}), Produced ({})".format(interface.is_consumed(1), interface.is_produced(1)))
+    print()
+    interface.is_waiting(0)
+    testwait(interface)
+    interface.step()
+    testwait(interface)
+    
+    '''
     station = Station(5)
     print(station.step(None))
     print(station.step(ArriveEvent.ARRIVE))
@@ -103,5 +152,6 @@ def test():
     testwait(station)
     print(station.step(ArriveEvent.LEAVE))
     print(station.step(None))
+    '''
 
 test()
